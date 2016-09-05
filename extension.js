2f7c2c44b073be69e94a0829729dbfe0b4bd17c3
Squrch <squrchgraphics@gmail.com>
2016-09-05 22:54:06 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,9 +105,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/squrch/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/squrch/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/squrch/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
