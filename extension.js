87b83bcbd6410d469cd8a685444e266a84482da5
Squrch <squrchgraphics@gmail.com>
2016-12-14 17:42:41 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -72,7 +72,7 @@
       voteSkipLimit: 5,
       historySkip: true,
       timeGuard: true,
-      maximumSongLength: 5,
+      maximumSongLength: 8,
       autodisable: true,
       commandCooldown: 30,
       usercommandsEnabled: true,
